504ddf5a0f149ac4261577c322dee4d79cde18a2
EverlastingLilflo <anjorin199@gmail.com>
2023-10-13 12:17:34 +0100

    wrote my code to delete each card

--- website/deleteProduct.php
+++ website/deleteProduct.php
@@ -1,6 +1,18 @@
 <?php 
 require "./extend/connectDb.php";
 
+// print_r($_GET);
+
+if (isset($_GET['deleteId'])){
+    $id = mysqli_escape_string($conn, $_GET['deleteId']);
+
+    $sql = "DELETE FROM pizza WHERE id = '$id'";
+
+
+    if (mysqli_query($conn, $sql)) {
+        header('location: index.php');
+    }
+}
 
 
 
