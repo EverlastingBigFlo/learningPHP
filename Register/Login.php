e827c93dd877fd1b32058c25591a22e58d34e775
EverlastingLilflo <anjorin199@gmail.com>
2023-10-17 15:30:17 +0100

    documented my code

--- Register/Login.php
+++ Register/Login.php
@@ -1,7 +1,5 @@
-
-
-
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,19 +7,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 </head>
+
 <body>
     <form action="./login-process.php" method="POST" novalidate>
         <h1>Login</h1>
+        
+        <?php if (isset($loginError)) : ?>
+            <p style="color: red;"><?php echo $loginError; ?></p>
+        <?php endif; ?>
+
         <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
         </div>
 
         <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" name="password" id="exampleInputPassword1">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+    </form>
 </body>
+
 </html>
--- Register/login-process.php
+++ Register/login-process.php
@@ -1,10 +1,12 @@
 <?php
+// Include your database connection code (e.g., connectDB.php).
 require __DIR__ . '/connectDB.php';
-// if (isset($_POST['submit'])) {
 
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+
+// Query the database to check if the user exists.
 
     $query = "SELECT * FROM `user` WHERE `email` = '$email'";
     $result = mysqli_query($conn, $query);
@@ -14,13 +16,18 @@ require __DIR__ . '/connectDB.php';
     }
 
     if (mysqli_num_rows($result) === 1) {
+// User exists, fetch their data.
+
         $row = mysqli_fetch_assoc($result);
 
-        // Verify the password.
+// Verify the password.
         if (password_verify($password, $row['password'])) {
             session_start();
+            // Password is correct. Set session variables to mark the user as logged in.
 
             $_SESSION['user_id'] = $row['id'];
+            
+// Redirect to a welcome page.
             header("Location: index.php");
         }else{
             die('invalid details');
@@ -29,6 +36,3 @@ require __DIR__ . '/connectDB.php';
     }else{
         die('invalid details');
     }
-
-
-?>
